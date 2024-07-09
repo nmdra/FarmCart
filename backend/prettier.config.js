3ce12d1968983b009569b057be6f0c8bc91bad8c
nmdra <nimendradilshan11@gmail.com>
2024-07-10 02:37:45 +0530

    🔧 Update config files

--- backend/prettier.config.js
+++ backend/prettier.config.js
@@ -1,8 +1,8 @@
 const config = {
-  trailingComma: "es5",
-  tabWidth: 4,
-  semi: false,
-  singleQuote: true,
-};
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+}
 
-export default config;
+export default config
