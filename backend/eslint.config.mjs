f3f5423df41aa5c35aa903b36917b893914ccb6b
nmdra <nimendradilshan11@gmail.com>
2024-07-10 20:34:32 +0530

    ➕ Add Jest related configs

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,8 +1,8 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import jest from 'eslint-plugin-jest'
 
 export default [
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
+  pluginJs.configs.recommended,jest.configs['flat/recommended'],
 ];
