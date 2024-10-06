d1a5640fc574563b55a43dc128d7ca7a5cf765a7
nmdra <nimendradilshan11@gmail.com>
2024-10-06 05:49:18 +0530

    🐛 Update

--- backend/routes/userShopRoute.js
+++ backend/routes/userShopRoute.js
@@ -19,16 +19,16 @@ router.get('/', cache('5 minutes'), getShops)
 
 // Route to fetch details of a specific shop by ID with caching
 // GET /api/shops/:id
-router.get('/random', getRandomProducts)
+router.get('/random', cache('3 minutes'), getRandomProducts)
 
 router.get('/:id', cache('5 minutes'), getShopById)
 
 // Route to fetch all products for a specific shop with caching
 // GET /api/shops/:id/products
-router.get('/:id/products', cache('10 minutes'), getShopProducts)
+router.get('/:id/products', cache('5 minutes'), getShopProducts)
 
 // Route to fetch a specific product from a shop by product ID with caching
 // GET /api/shops/:id/products/:productId
-router.get('/:id/products/:productId', cache('10 minutes'), getShopProductById)
+router.get('/:id/products/:productId', cache('5 minutes'), getShopProductById)
 
 export default router
