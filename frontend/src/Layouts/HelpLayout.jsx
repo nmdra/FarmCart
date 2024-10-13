d8753fba7279569a48558e7447ae44831590fbdc
aweeshathavishanka <aweesha.info@gmail.com>
2024-10-13 23:08:55 +0530

    Demo Live chat Add

--- frontend/src/Layouts/HelpLayout.jsx
+++ frontend/src/Layouts/HelpLayout.jsx
@@ -1,13 +1,23 @@
 import { Outlet } from 'react-router-dom'
 import NavBar from '../Components/Help/NavBar'
 import HelpFooter from '../Components/Help/HelpFooter'
+import HelpChatIcon from '../Components/Help/HelpChatIcon'
 
 function HelpLayout() {
+    const user = {
+        id: '123',
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        createdAt: '2024-01-01T12:34:56.000Z',
+    }
     return (
         <div>
             <NavBar />
             <div>
                 <Outlet />
+                <div className="App">
+                    <HelpChatIcon user={user} />
+                </div>
             </div>
             <HelpFooter />
         </div>
